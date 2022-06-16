8611d6dd37ab9417c039e810e15448994b37bfe6
Lea <dev.venezia@gmail.com>
2022-06-16 23:29:24 +0100

    Adding cors whitelisted.

--- src/server.js
+++ src/server.js
@@ -32,7 +32,21 @@ const question = require("./routes/question/question");
 const cart = require("./routes/cart/cart");
  */
 
-app.use(cors('*'));
+// Options Cors
+const whitelist = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(",") : [];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+// Middlewares
+app.use(cors(corsOptions));
 app.use(passport.initialize())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
